e94e1cb8b8c2400a717d64ce2e48b33ebe1b7411
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-02-12 17:37:28 -0300

    feat: add methods and the UI for the selected location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,4 +38,10 @@ export class AppComponent {
           laundry: false,
       }
  ]
+
+ selectedLocation: HousingLocation | undefined
+
+ updateSelectedLocation(location: HousingLocation) {
+     this.selectedLocation = location
+ }
 }
