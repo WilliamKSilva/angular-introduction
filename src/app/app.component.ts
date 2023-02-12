f39aa436089f8aee7e5948294e3f502a9065bbdd
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-02-12 17:01:43 -0300

    feat: add HousingLocation interface and properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { HousingLocation } from './housing-location';
 
 @Component({
   selector: 'app-root',
@@ -8,40 +7,4 @@ import { HousingLocation } from './housing-location';
 })
 export class AppComponent {
   title = 'fairhouse';
-
-  housingLocationList: HousingLocation[] = [
-    {
-      name: "Acme Fresh Start Housing",
-      city: "Chicago",
-      state: "IL",
-      photo: "../assets/housing-1.jpg",
-      availableUnits: 4,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      name: "A113 Transitional Housing",
-      city: "Santa Monica",
-      state: "CA",
-      photo: "../assets/housing-2.jpg",
-      availableUnits: 0,
-      wifi: false,
-      laundry: true,
-    },
-    {
-      name: "Warm Beds Housing Support",
-      city: "Juneau",
-      state: "AK",
-      photo: "../assets/housing-3.jpg",
-      availableUnits: 1,
-      wifi: false,
-      laundry: false,
-    }
-  ];
-
-  selectedLocation: HousingLocation | undefined;// = this.housingLocationList[0];
-
-  updateSelectedLocation(location: HousingLocation) {
-    this.selectedLocation = location;
-  }
 }
